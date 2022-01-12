bf1f939a927211b95ec031a6a5dfa8277f4da979
guidsdo <guidsdo@users.noreply.github.com>
2022-01-12 03:20:36 +0100

    WIP tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
-use ferris_says;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let stdout = io::stdout();
-    let message = String::from("Hello world");
-    let width = message.chars().count();
+    println!("Guess the number!");
 
-    let mut writer = io::BufWriter::new(stdout.lock());
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    ferris_says::say(message.as_bytes(), width, &mut writer).unwrap_or({});
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("Please guess again what the number is.");
+    }
+
+    println!("Exiting now...")
 }
